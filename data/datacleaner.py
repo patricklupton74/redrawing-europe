f3890244bf98885f3a9a12f7af3f2c5f85446ae6
Patrick Lupton <patricklupton74@gmail.com>
2026-03-29 23:13:52 +0100

    Data filtering complete, final europeancities.csv made

--- data/datacleaner.py
+++ data/datacleaner.py
@@ -1,10 +1,34 @@
 #objectives:
-#keep only cities in europe ()
+#keep only cities in europe (removed trans-continental and cyprus) (✓)
 #keep only city, country, lng, lat attributes (✓)
+#remove tiny microstates (✓)
+
 import pandas as pd
 
-usecols = ['city','lat','lng','country']
+usecols = ['city','lat','lng','country','population']
 
-cities = pd.read_csv("data/worldcities.csv", index_col = 0,usecols = usecols)
-cities.to_csv("data/onlycorrectcolumns.csv")
+cities = pd.read_csv("data/worldcities.csv", usecols = usecols)
+cities.to_csv("data/onlycorrectcolumns.csv", index = False)
 
+'''
+for country in cities["country"].unique():
+   print(country)
+'''
+
+european_countries = [
+    "Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina",
+    "Bulgaria", "Croatia", "Czechia", "Denmark", "Estonia",
+    "Finland", "France", "Germany", "Greece", "Hungary", "Ireland",
+    "Italy", "Latvia", "Lithuania", "Luxembourg",
+    "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia",
+    "Norway", "Poland", "Portugal", "Romania", "Serbia",
+    "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine",
+    "United Kingdom"
+]
+
+cities = cities[cities["country"].isin(european_countries)]
+cities = cities.groupby("country").apply(lambda x: x.nlargest(min(len(x), 100), 'population')).reset_index(drop = False)
+cities = cities.drop(columns=['level_1'])
+cities.to_csv("data/europeancities.csv", index = False)
+#print(cities.head())
+#print(cities.columns)
