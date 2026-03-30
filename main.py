d56dc72c58f8f0fc4a0b9ed71a4394c9d0d87f72
Patrick Lupton <patricklupton74@gmail.com>
2026-03-30 19:15:07 +0100

    KMeans tested & working, demo images added in /images

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from algorithms.knn import KNN
+from algorithms.kmeans import KMeans
 from utils import train_test_split
 
 cities = pd.read_csv("data/europeancities.csv")
 
 #kNN --------------------------------------------------------------------------------------
-train_size = 0.2
+train_size = 0.7
 pos_training, label_training, pos_input, label_input = train_test_split(cities, train_size)
 
-knn = KNN(5)
+knn = KNN()
 knn.train(pos_training, label_training)
 predictions = knn.predict(pos_input)
 
@@ -24,4 +25,10 @@ print(f"{accuracy}%")
 print(f"k: {knn.k}")
 print(f"Training size: {len(pos_training)}")
 print(f"Input size: {len(pos_input)}")
+#------------------------------------------------------------------------------------------
+
+#K-Means ----------------------------------------------------------------------------------
+kmeans = KMeans(38, cities)
+results = kmeans.run()
+#results is an array of k clusters, each cluster being an array of locations corresponding to cities in that cluster
 #------------------------------------------------------------------------------------------
